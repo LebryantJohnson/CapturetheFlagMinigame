544b92654b14cadadd409dd8f7f89021db665857
LebryantJohnson <69935607+LebryantJohnson@users.noreply.github.com>
2022-11-16 00:01:43 -0800

    added some startup logic

--- src/main/java/me/lebryant/capturethecastle/CaptureTheCastle.java
+++ src/main/java/me/lebryant/capturethecastle/CaptureTheCastle.java
@@ -3,6 +3,8 @@ package me.lebryant.capturethecastle;
 import me.lebryant.capturethecastle.commands.AdminGUICommand;
 import me.lebryant.capturethecastle.commands.selfcommand;
 import me.lebryant.capturethecastle.events.AdminGUI_Clicks;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CaptureTheCastle extends JavaPlugin {
@@ -18,6 +20,14 @@ public final class CaptureTheCastle extends JavaPlugin {
         getServer().getPluginManager().registerEvents( new AdminGUI_Clicks(), this);
 
         this.saveDefaultConfig();
+
+
+        //send messages to console
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "-----------========      Capture the Castle     ========-----------");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "- Welcome to Capture the Castle Plugin. View our github for help: https://github.com/LebryantJohnson/CapturetheFlagMinigame");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "- If an error occurs you are supposed to contact the developer and DO NOT try out things yourself.");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "- Report bugs to the developer if there are any.");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "-----------=========================-----------");
     }
 
     @Override
